c4ea9255234c01deff528f4ab9054cc36f6dfaaa
Zwel <zwelhtetyandev@gmail.com>
2026-03-15 09:26:25 +0630

    feat: initial commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute("/")({ component: App });
 function App() {
 	return (
 		<main>
-			<h1>HI</h1>
+			<h1 className="text-blue-500 font-bold">HI</h1>
 		</main>
 	);
 }
