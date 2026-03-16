59509ad1f85624f7fff8da0a4d2d7aa93156c950
Zwel <zwelhtetyandev@gmail.com>
2026-03-16 12:19:12 +0630

    chore: greeting text 👋

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,25 +7,26 @@ export const Route = createFileRoute("/")({ component: App });
 function App() {
 	return (
 		<main>
-			<div className="flex min-h-svh p-6">
-				<div className="flex min-w-0 max-w-md flex-col gap-4 leading-loose">
-					<div>
-						<h1 className="font-medium">Project ready!</h1>
-						<p>You may now add components and start building.</p>
-						<p>We&apos;ve already added the button component for you.</p>
-						<Button
-							className="mt-2"
-							onClick={() =>
-								toast.message("Hey 👋", {
-									description: "Welcome to Tanstack SPA Starter",
-									position: "top-center",
-								})
-							}
-							size="lg"
-						>
-							Button
-						</Button>
-					</div>
+			<div className="flex min-h-svh items-center justify-center p-6">
+				<div className="flex min-w-0 max-w-md flex-col leading-loose">
+					<h1 className="font-medium">Tanstack SPA Starter!</h1>
+					<p className="text-sm">
+						A minimal React + TanStack SPA boilerplate designed to help you
+						efficiently kickstart your next project.
+					</p>
+
+					<Button
+						className="mt-2 justify-self-center"
+						onClick={() =>
+							toast.message("Hey 👋", {
+								description: "Welcome to Tanstack SPA Starter",
+								position: "top-center",
+							})
+						}
+						size="lg"
+					>
+						Welcome 👋
+					</Button>
 				</div>
 			</div>
 		</main>
